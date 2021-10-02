c32abd9bb6cd9b9d8781db039f9b25345f84b846
PabloAndroid33 <pabloAndroid44@gmail.com>
2021-10-01 19:41:08 -0500

    docs agregados

---
+++ docs/precache-manifest.7dd7f003ffc25865a4f15e66927e346a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c8b0c3780ba29b3220090de1337286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f9dfeeca104b1c824d7",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9dfeeca104b1c824d7",
+    "url": "/static/js/main.5aa1328f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
